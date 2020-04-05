02db800ed69699d94c4d3721cb0dd9272501d453
Krassilnikov-AV <61631173+Krassilnikov-AV@users.noreply.github.com>
2020-04-05 18:05:04 +0300

    Update MessageBean.java

--- MessDataBase-ejb/src/java/lab4gr1/ejb/MessageBean.java
+++ MessDataBase-ejb/src/java/lab4gr1/ejb/MessageBean.java
@@ -1,6 +1,4 @@
-
 package lab4gr1.ejb;
-
 
 import java.util.List;
 import javax.annotation.Resource;
@@ -9,42 +7,59 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 
-
+//компонент работает с ДБ непосредственно
+// Компонентно-определяющая аннотация для сессионного компонента без сохранения состояния.
 @Stateless
 public class MessageBean implements MessageBeanLocal {
 
     // сказать где БД
-    @Resource(name="jdbs/messDataBase") 
-    private DataSource ds;
-    @PersistenceContext
-    private EntityManager em;
-    
+    @Resource(name = "jdbs/messDataBase")
+    private DataSource ds;           //
+    @PersistenceContext             //Выражает зависимость от управляемого контейнером EntityManager и связанного с ним контекста персистентности.
+    private EntityManager em;       // Интерфейс, используемый для взаимодействия с контекстом постоянства.
+ //   private EntityManager emn;     // 
+// обращение к классу - таблице Smessage и извлечение списка из таблицы
+    @Override
+    public String[] getMessageList() {   
+        System.out.println(" Вызван метод getMessageList()");
+        List<Smessage> list;                      // создание списка 
+
+        list = (List<Smessage>) em.createNamedQuery("Smessage.getAll").getResultList();
+        String[] array = new String[list.size()];
+        int i = 0;
+        for (Smessage sm : list) {
+            array[i++] = sm.getMessage();
+        }
+        System.out.println("метод getMessageList() завершился " + array);
+
+        return array;
+    }
 
     @Override
-    public String[] getMessageList() {
-       System.out.println(" Вызван метод getMessageList()");
-       List<Smessage> list;
-       
-       list = (List<Smessage>)em.createNamedQuery("Smessage.getAll").getResultList();
-       String[] array = new String[list.size()];
-       int i = 0;
-       for(Smessage sm : list) {
-           array[i++] = sm.getMessage();
+    public Integer[] getSumm() {
+        System.out.println("Вызван метод long GetSumm()");
+
+        List<Nmessage> nlist;
+        
+        nlist = (List<Nmessage>) em.createNamedQuery("Nmessage.getAll").getResultList();
+        Integer[] numarr = new Integer[nlist.size()];
+        int i=0;
+        for(Nmessage nm : nlist) {
+            numarr[i++] = nm.getMessage();
+        }
+         System.out.println("метод getSumm() завершился " + numarr);
+        
+        return numarr;
     }
-    System.out.println("метод getMessageList() завершился " + array);
-      
-    return array;
-}
+// метод сохранения в БД строковых сообщений 
     
-    @Override
-    public long longGetSumm() {
-        return 0L;
-    }
-// такой же и на числовые
     @Override
     public void addSmessage(Smessage message) {
-       em.persist(message);
+        em.persist(message);            // 
     }
-    
-    
+
+    @Override
+    public void addNmessage(Nmessage message) {
+        em.persist(message);        // persist(Object entity) делает экземпляр управляемым и постоянным.  
+    }
 }
