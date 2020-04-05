652033cfffd11c3b9668820dbff21a96b9e7aec0
Krassilnikov-AV <61631173+Krassilnikov-AV@users.noreply.github.com>
2020-04-05 18:08:33 +0300

    Update TextMessageBean.java

--- MessDataBase-ejb/src/java/lab4gr1/ejb/TextMessageBean.java
+++ MessDataBase-ejb/src/java/lab4gr1/ejb/TextMessageBean.java
@@ -1,6 +1,4 @@
-
 package lab4gr1.ejb;
-
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,8 +11,8 @@ import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
 /**
+ * Бин для обработки входящих строковых сообщений
  *
- * @author Alexk
  */
 @MessageDriven(activationConfig = {
     @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/textmessage")
@@ -22,28 +20,29 @@ import javax.jms.TextMessage;
         @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
 })
 public class TextMessageBean implements MessageListener {
-    
-    @EJB
+
+    @EJB                // 
     private MessageBeanLocal messageBean;
-    
+
     public TextMessageBean() {
     }
-    
-    @Override
+
+    @Override                // переопределяется из интерфейса MessageListener
     public void onMessage(Message message) {
-        System.out.println("Вызван метод onMessage()");
-        TextMessage tm = (TextMessage)message;
-        String text = "";
+        System.out.println("Вызван метод onMessage()");    // оповещение вызова
+        TextMessage tm = (TextMessage) message;   // создается ссылочная переменная интерфейса TextMessage
+        String text = "";                    // создается строковая ссылочная переменная 
         try {
-            text = tm.getText();
-            System.out.println("Получено сообщение: " + text);
-    //создание и отправление в БД объекта типа Entity
-            Smessage sm = new Smessage();
-            sm.setMessage(text);
-            messageBean.addSmessage(sm);
+            text = tm.getText();              //  Получает строку, содержащую данные сообщения.
+            System.out.println("Получено сообщение: " + text);    // выводится сообщение
+            //создание и отправление в БД объекта типа Entity
+            Smessage sm = new Smessage();    // создается ссылочная переменая класса сущности Entity 
+            sm.setMessage(text);             // переменная получает сообщение 
+            messageBean.addSmessage(sm);     // переменная messageBean добавляет сообщение в метод addSmessage() 
+            //интерфейса MessageBeanLocal, который переопределяется в классе MessageBean и 
+            // делает экземпляр управляемым и постоянным
         } catch (JMSException ex) {
             Logger.getLogger(TextMessageBean.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        }
     }
+}
