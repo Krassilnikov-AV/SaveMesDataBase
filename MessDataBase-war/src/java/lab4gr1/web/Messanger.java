9320a20ad96d4c4382e4361d91e7c4b88c2420e4
Krassilnikov-AV <61631173+Krassilnikov-AV@users.noreply.github.com>
2020-04-07 20:56:34 +0300

    Update Messanger.java

--- MessDataBase-war/src/java/lab4gr1/web/Messanger.java
+++ MessDataBase-war/src/java/lab4gr1/web/Messanger.java
@@ -49,8 +49,9 @@ public class Messanger extends HttpServlet {
         String choice = request.getParameter("choice");      // 
 // добавление кнопок получения списка сообщений и суммы значений        
         //       boolean list = request.getParameter("listmess") == null;
-        boolean summ = request.getParameter("addmess") != null;
-
+        boolean summ = request.getParameter("total") != null;
+        boolean list = request.getParameter("list") != null;
+   
         try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
@@ -61,20 +62,21 @@ public class Messanger extends HttpServlet {
             out.println("<h1>Servlet Messanger at " + request.getContextPath() + "</h1>");
             out.println("<h1>Получена информация: " + info + "</h1>");
             request.setAttribute(info, info);
-            Connection conn;   // цели соединения: 
-            // 1. инкапсулирует открытое соединение с провайдером JMS. Обычно он представляет собой открытый сокет TCP / IP
-            //между клиентом и программным обеспечением поставщика услуг.                          
-            Session session;     // 
+            Connection conn = null; // цели соединения:  1. инкапсулирует открытое соединение с провайдером JMS.
+                              //  Обычно он представляет собой открытый сокет TCP / IP 
+                               //между клиентом и программным обеспечением поставщика услуг.
+            Session session;  // 
             if ("text".equals(choice)) {
                 out.println("<h1>It must be a text</h1>");
-// обработка текстового сообщения            
-                try {
+                // обработка текстового сообщения 
+                try { 
+//получение соединения
                     conn = textFactory.createConnection();
-                    session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);    // false - отсутствие транзакций, режим автоподтверждения
-                    MessageProducer mp = (MessageProducer) session.createProducer(textQueue); // Создает MessageProducer для отправки сообщений в указанный пункт назначения.
-                    TextMessage tm = session.createTextMessage(info);      // передача текста из формы
-                    mp.send(tm);              // отправка полученного собщения
+                    session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE); // false - отсутствие транзакций, режим автоподтверждения 
+                    MessageProducer mp = (MessageProducer) session.createProducer(textQueue); // Создает MessageProducer для отправки сообщений в указанный пункт назначения. 
 
+                    TextMessage tm = session.createTextMessage(info); // передача текста из формы 
+                    mp.send(tm);         // отправка полученного собщения
                     out.println("<h1>Your messager is entered</h1>");
                     mp.close();
                     session.close();
@@ -82,17 +84,16 @@ public class Messanger extends HttpServlet {
                 } catch (JMSException ex) {
                     out.println("<h1>Problem sending the value</h1>");
                 }
-            } else {
+            }
+            if ("number".equals(choice)) {
                 out.println("<h1>It must be a number</h1>");
-// обработка числовых значений
+                // обработка числовых значений
                 try {
-                    conn = numFactory.createConnection();          // 
+                    conn = numFactory.createConnection(); // 
                     session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-                    MessageProducer mp = (MessageProducer) session.createProducer(numQueue);   //отправка сообщений в пункт назначения. 
-                    ObjectMessage tm = session.createObjectMessage(info);      // передача текста из формы
-                    mp.send(tm);
-// если не выкинет исключение, то...
-
+                    MessageProducer mp = (MessageProducer) session.createProducer(numQueue); //отправка  сообщений в пункт назначения. 
+                    ObjectMessage tm = session.createObjectMessage(info); // передача текста из формы //
+                    mp.send(tm); // если не выкинет исключение, то... 
                     out.println("<h1> Your number is entered </h1>");
                     mp.close();
                     session.close();
@@ -102,30 +103,28 @@ public class Messanger extends HttpServlet {
                 }
             }
             out.println("<h1>Сообщения из базы: </h1>");
-            if ("text".equals(choice)) {           // если значение поля "текст", тогда создается список из базы текстовых сообщений
+            if (list == true) { // если значение поля "текст", тогда создается список из базы текстовых сообщений 
                 String[] messages = messageBean.getMessageList();
                 for (String m : messages) {
                     out.println("<h2>" + m + "/<h2>");
                 }
-            } else if ("number".equals(choice)) {                             // иначе создается список из базы чисел
-// возвращение списка чисел             
-                Integer[] message = messageBean.getLitsSumm();
-                for (Integer m : message) {
-
-                    out.println("<h2>" + m + "/<h2>");
-                }
-// возвращение суммы всех введенных чисел
-
-                int total = messageBean.getSumm();               
-                out.println( "<h2>"+ "total: " + total + "/<h2>");
-            }
-
+            } else if (summ == true) { // иначе создается список из базы чисел 
+                // возвращение списка чисел 
+//                Integer[] message = messageBean.getLitsSumm();
+//                for (Integer m : message) {
+//                    out.println("<h2>" + m + "/<h2>");
+//                }
+                // возвращение суммы всех введенных чисел
+                int total = messageBean.getSumm(); //
+                out.println("<h2>" + "total: " + total + "/<h2>");
+            } //
             out.println("</body>");
             out.println("</html>");
         }
+
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
